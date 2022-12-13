bec23042ac6a2ade2d9f7e7bbbb802136f33d07f
Nero <resqnetm@gmail.com>
2022-12-13 03:04:04 +0300

    Add codec test

--- maid/src/main.rs
+++ maid/src/main.rs
@@ -1,3 +1,10 @@
+use maid_codec::arm64::decoder::*;
+
 fn main() {
-    println!("Hello, world!");
+    let buffer = include_bytes!("../../data/add.bintest");
+    let mut decoder = BufferedDecoder::new(buffer);
+
+    while let Ok(insn) = decoder.decode_next() {
+        dbg!(insn);
+    }
 }
