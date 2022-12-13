2c5a9555f5e81b52c61aa5e51fe6e0c2c9c9f65b
Nero <resqnetm@gmail.com>
2022-12-13 03:03:27 +0300

    Add bare metal compile script & example data to it

---
+++ barecc.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+ARM64_CC=aarch64-unknown-linux-gnu-gcc
+OBJCOPY=aarch64-unknown-linux-gnu-objcopy
+
+function help() {
+    echo "$0: usage: $0 path/to/file.c"
+    exit
+}
+
+function invoke() {
+    echo ">> $@"
+    $@
+}
+
+if [[ -z "$1" ]]; then
+    help
+fi
+
+file="$1"
+output="${file%.c}.bintest"
+
+echo "$output - $file"
+
+invoke $ARM64_CC -ffreestanding -nostdlib -Wl,-Ttext=0x0 $file -o $output
+invoke $OBJCOPY -O binary $output
+
