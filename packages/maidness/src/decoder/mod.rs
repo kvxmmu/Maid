0138ffc86fb728794885fc1ab7de48b51de031d0
Nero <resqnetm@gmail.com>
2022-12-29 14:32:38 +0300

    Add utils from previous codebase & add data_processing_immediate table

--- packages/maidness/src/decoder/mod.rs
+++ packages/maidness/src/decoder/mod.rs
@@ -1,4 +1,5 @@
 mod fns;
+mod utils;
 
 pub mod block;
 pub mod top_level;
--- packages/maidness/src/decoder/top_level/data_processing_immediate.rs
+++ packages/maidness/src/decoder/top_level/data_processing_immediate.rs
@@ -11,14 +11,14 @@ use crate::{
 
 #[rustfmt::skip]
 static DP_IMMEDIATE_TABLE: [TableFn; 8] = [
-    stub,   // 0b000
-    stub,   // 0b001
-    stub,   // 0b010
-    stub,   // 0b011
-    stub,   // 0b100
-    stub,   // 0b101
-    stub,   // 0b110
-    stub,   // 0b111
+    stub,   // 0b000 (PC-rel.addressing)
+    stub,   // 0b001 (PC-rel.addressing)
+    stub,   // 0b010 (Add/subtract - immediate)
+    stub,   // 0b011 (Add/subtract - immediate, with tags)
+    stub,   // 0b100 (Logical - immediate)
+    stub,   // 0b101 (Move wide - immediate)
+    stub,   // 0b110 (Bitfield)
+    stub,   // 0b111 (Extract)
 ];
 
 pub(crate) fn jump_dp_immediate(unit: &mut Unit, block: Block) {
